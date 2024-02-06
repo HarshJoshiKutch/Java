0c174c3b9f211b64dccdab249c7d2e8e9bcb4ad2
JOSHI HARSH <124500473+HarshJoshiKutch@users.noreply.github.com>
2024-02-06 23:26:25 +0530

    Add files via upload

---
+++ patterns/invertedandrotatedhalf.java
@@ -0,0 +1,18 @@
+public class invertedandrotatedhalf {
+    public static void inverted_and_rotated_half(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        inverted_and_rotated_half(5);
+    }
+
+}
